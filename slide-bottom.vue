af46a3febbc0f5dcec6f6f773f0b0b251d16003c
Nicolas Fondacci <n.fondacci@gmail.com>
2025-05-03 19:32:49 +0200

    💫 Add default slide transitions

--- slide-bottom.vue
+++ slide-bottom.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="$nav.currentLayout !== 'cover'" class="fixed right-5 bottom-5 text-slate-400">
+    <div v-if="!['intro', 'cover'].includes($nav.currentLayout)" class="fixed right-5 bottom-5 text-slate-400">
         <SlideCurrentNo />
     </div>
 </template>
